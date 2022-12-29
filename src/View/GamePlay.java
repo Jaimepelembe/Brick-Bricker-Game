ec0c2c57c4a5344ea39a590b199dae1c8baa30c9
Jaimepelembe <105989003+Jaimepelembe@users.noreply.github.com>
2022-12-29 21:46:32 +0200

    Criei os 4 Niveis do Jogo
    Coloquei os 4 niveis do jogo
    Criei a classe de Som

--- src/View/GamePlay.java
+++ src/View/GamePlay.java
@@ -13,6 +13,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -23,15 +28,17 @@ import javax.swing.Timer;
 public class GamePlay extends JPanel implements ActionListener, KeyListener {
 
     private boolean play = false;
-    private boolean nextLevel = false;
+    //private boolean nextLevel = false;
+    private int level=2;
     private Timer timer;
-    private int delay = 8;
+    private int delay = 1;
     private int width = new MainScreen("").getWindowsWidth();
     private int height = new MainScreen("").getWindowsHeight();
     private MapGenerator map;
     private Paddle paddle;
     private Ball ball;
     private Color background = Color.black;
+    private Music music;
 
     public GamePlay() {
         addKeyListener(this);
@@ -42,6 +49,7 @@ public class GamePlay extends JPanel implements ActionListener, KeyListener {
         paddle = new Paddle();
         ball = new Ball();
         map = new MapGenerator(3, 7);
+        music= new Music();
     }
 
     public GamePlay(String str) {
@@ -69,7 +77,7 @@ public class GamePlay extends JPanel implements ActionListener, KeyListener {
         ball.draw(g);
         ball.setMap(map);
 
-        Score(g);
+        ScoreAndLevel(g);
 
         //Game Over
         if (ball.getBallposY() >= height) {
@@ -77,19 +85,9 @@ public class GamePlay extends JPanel implements ActionListener, KeyListener {
         }
 
         //You won
-        YouWon(g);
-
-    }
-
-    public void RestartGame() {
-        ball.setBallposX(width / 2);
-        ball.setBallposY(height / 2);
-        ball.setBallXdir(-3);
-        ball.setBallYdir(-3);
-        ball.setScore(0);
-        map = new MapGenerator(3, 7);
-        paddle.setPlayerX(width / 2);
-
+        if(verifyEndLevel()){
+        YouWon(g);}
+     
     }
 
     public void Level2() {
@@ -100,6 +98,40 @@ public class GamePlay extends JPanel implements ActionListener, KeyListener {
         map = new MapGenerator(4, 8);
         map.setMapColor(Color.MAGENTA);
         paddle.setPlayerX(width / 2);
+        paddle.setSpeedPuddle((int) (width * 0.030));
+    }
+      
+    public void Level3() {
+        ball.setBallposX(width / 2);
+        ball.setBallposY(height / 2);
+        ball.setBallXdir(-5);
+        ball.setBallYdir(-5);
+        map = new MapGenerator(5, 9);
+        map.setMapColor(Color.ORANGE);
+        paddle.setPlayerX(width / 2);
+        paddle.setSpeedPuddle((int) (width * 0.038));
+    }
+    
+    public void Level4(){
+    ball.setBallXdir(width/2);
+    ball.setBallYdir(height/2);
+    ball.setBallXdir(-6);
+    ball.setBallYdir(-6);
+    map =new MapGenerator(6,10);
+    map.setMapColor(Color.YELLOW);
+    paddle.setPlayerX(width/2);
+    paddle.setSpeedPuddle((int) (width * 0.048));
+    }
+    
+    public void RestartGame() {
+        ball.setBallposX(width / 2);
+        ball.setBallposY(height / 2);
+        ball.setBallXdir(-3);
+        ball.setBallYdir(-3);
+        ball.setScore(0);
+        map = new MapGenerator(3, 7);
+        paddle.setPlayerX(width / 2);
+
     }
 
     public void gameOver(Graphics g) {
@@ -121,17 +153,23 @@ public class GamePlay extends JPanel implements ActionListener, KeyListener {
         g.drawString("Your Score: " + ball.getScore(), x + 70, y2);
         g.setColor(Color.YELLOW);
         g.setFont(new Font("arial", Font.BOLD, 30));
-        g.drawString("Press Enter to Restart!", x + 20, y3);
+        g.drawString("Press SPACE to Restart!", x + 20, y3);
 
     }
 
+    public boolean verifyEndLevel(){
+    if(map.getTotalBricks()<=0){
+    return true;
+    }
+    return false;
+}
+    
     public void YouWon(Graphics g) {
-        if (map.getTotalBricks() <= 0) {
             paddle.setPlay(false);
             paddle.setPlayerX(0);
             ball.setBallXdir(0);
             ball.setBallYdir(0);
-            nextLevel = true;
+            //nextLevel = true;
 
             g.setColor(Color.blue);
             g.setFont(new Font("arial", Font.BOLD, 50));
@@ -139,29 +177,28 @@ public class GamePlay extends JPanel implements ActionListener, KeyListener {
             int y1 = (height / 2) - 100;
             int y2 = (height / 2) - 40;
             int y3 = (height / 2) + 15;
-            int y4 = (height / 2) + 60;
+            
             g.drawString("You Win!", x + 70, y1);
 
             g.setColor(Color.WHITE);
             g.setFont(new Font("arial", Font.BOLD, 50));
             g.drawString("Your Score: " + ball.getScore(), x + 20, y2);
-
+            
+            if(level<4){
             g.setColor(Color.YELLOW);
             g.setFont(new Font("arial", Font.BOLD, 30));
-            g.drawString("Press Space to Go the next Level", x + 20, y4);
-
-            g.setColor(Color.orange);
-            g.drawString("Press Enter to Restart", x + 20, y3);
-
-        }
-
+            g.drawString("Press ENTER to GO Level "+level, x+20 , y3);}
     }
 
-    public void Score(Graphics g) {
+    public void ScoreAndLevel(Graphics g) {
         if (paddle.isPlay()) {
             g.setColor(Color.WHITE);
             g.setFont(new Font("calibre", Font.BOLD, 25));
             g.drawString("Score: " + ball.getScore(), width / 2, 22);
+            
+            g.setColor(Color.LIGHT_GRAY);
+            g.setFont(new Font("calibre", Font.BOLD, 22));
+            g.drawString("Level: " + (level-1), 250, 22);
         }
 
     }
@@ -175,7 +212,7 @@ public class GamePlay extends JPanel implements ActionListener, KeyListener {
 
         if (paddle.isPlay()) {
             ball.moveBall(paddle);
-
+       
         }
         repaint();
     }
@@ -186,20 +223,33 @@ public class GamePlay extends JPanel implements ActionListener, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
-        paddle.keyPressed(e);//Movements of the puddle
+        //Movements of the puddle
+        paddle.keyPressed(e);
 
         //Restar the game
-        if (e.getKeyCode() == KeyEvent.VK_ENTER && !paddle.isPlay()) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE && !paddle.isPlay() && !verifyEndLevel()) {
             RestartGame();
+            level=2;
             ball.setScore(0);
         }
 
-        //Go to nexte Level
-        if (e.getKeyCode() == KeyEvent.VK_SPACE && nextLevel) {
+        //Go to next Level
+        //Level 2
+        if (e.getKeyCode() == KeyEvent.VK_ENTER && verifyEndLevel() && level==2) {
+            level=3;
             Level2();
+            
         }
-
+        
+        //Level 3
+        if(e.getKeyCode()==KeyEvent.VK_ENTER && verifyEndLevel() && level==3){
+        level=4;
+        Level3();
+        }
+        
+        if(e.getKeyCode()== KeyEvent.VK_ENTER && verifyEndLevel() && level==4){
+        Level4();
+        }
         repaint();
     }
 
